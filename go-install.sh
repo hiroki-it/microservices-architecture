9a6d0e57568d9dc8ad0fca74aa7ac54d556e4270
hiroki-it <hasegawafeedshop@gmail.com>
2024-06-15 23:28:09 +0900

    ツールのバージョンを管理できるようにした

---
+++ go-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+echo 'export PATH="$PATH:'$(go env GOPATH)'/bin"' >> ~/.zshrc
+source ~/.zshrc
