01a7d169c24ce6154e5ca6b9b3117ba5905d4f33
hiroki-it <hasegawafeedshop@gmail.com>
2024-06-15 23:55:12 +0900

    bffサービスを作成した

---
+++ application/bff/index.js
@@ -0,0 +1,54 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { typeDefs } from './schema.js'
+import { resolvers } from './resolver.js'
+import { CatalogueDataSource }  from './datasource/catalogue.js' 
+import { OrderDataSource }  from './datasource/order.js' 
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+
+// Expressサーバとの統合
+const app = express();
+
+
+// Expressサーバーへの受信リクエストを処理するhttpServerの設定
+const httpServer = http.createServer(app);
+
+// ApolloServer 初期化用の処理
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+// ApolloServerの起動
+await server.start()
+
+// サーバーをマウントするパスの指定
+app.use(
+  '/graphql',
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => {
+        return {
+          dataSources: {
+            catalogueApi: new CatalogueDataSource(),
+            orderApi: new OrderDataSource()
+          }
+        }
+      }
+  }),
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
+
+app.get('/health', (req, res) => {
+  res.status(200).send('Okay!');
+});
